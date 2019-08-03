1c2c6960fd9936fb99c8246c2198f1205baa3b56
jsartbaeva <jsartbaeva91@gmail.com>
2019-08-03 13:01:48 -0500

    added private subnet

--- public_sg.tf
+++ public_sg.tf
@@ -35,3 +35,36 @@ resource "aws_security_group" "public" {
       Dept ="${var.Dept}"
   }
 }
+
+resource "aws_security_group" "privateDB" {
+  name        = "privateDB"
+  description = "Allow TLS inbound traffic"
+  vpc_id      = "${aws_vpc.dev.id}"
+
+  ingress {
+    from_port   = 3306
+    to_port     = 3306
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+    
+  egress {
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
+    cidr_blocks     = ["0.0.0.0/0"]
+    }
+  tags ={
+      Name ="${var.Name}.privateDB"
+      Env ="${var.Env}"
+      Created_by ="${var.Created_by}"
+      Dept ="${var.Dept}"
+  }
+}
