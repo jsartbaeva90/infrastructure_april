a4d785d49b3fec1d2c5fc6e372f04a2bdb43a034
jsartbaeva <jsartbaeva91@gmail.com>
2019-08-03 10:31:57 -0500

    update bucket

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
     backend "s3"{
-        bucket ="infrastructure-april-zhazgul"
+        bucket ="infrastructure-april-zhazgul1"
         region = "eu-west-1"
         key = "infra/state"
     }
