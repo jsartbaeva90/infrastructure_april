9ccb346d8cd3b51fdc5b4b3b21f68604a4cf34ca
jsartbaeva <jsartbaeva91@gmail.com>
2019-08-04 15:59:28 -0500

    added tags to asg

--- asg.tf
+++ asg.tf
@@ -19,7 +19,7 @@ module "wordpress" {
   max_size                  = 128
   desired_capacity          = 4
   wait_for_capacity_timeout = 0
-  tags = {
+  tags_as_map = {
       Name ="${var.Name}"
       Env ="${var.Env}"
       Created_by ="${var.Created_by}"
