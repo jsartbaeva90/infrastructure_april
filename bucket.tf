66e701f44f1cdf2ae69c275c315ab6d07ed7d435
jsartbaeva <jsartbaeva91@gmail.com>
2019-08-04 14:15:48 -0500

    added bucket

---
+++ bucket.tf
@@ -0,0 +1,4 @@
+resource "aws_s3_bucket" "bucketname" {
+    bucket = "infrastructure-april-zhazgul-19a"
+  
+}
---
+++ dont_create.tf
@@ -0,0 +1,3 @@
+resource "aws_s3_bucket" "bucketnameb" {
+    bucket = "infrastructure-april-zhazgul-19a"
+}
